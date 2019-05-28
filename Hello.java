346c893d0225a0620a55e88935eee7b73fbac4be
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 07:40:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Main{
 public static void main(String args[]){
 Scanner s=new Scanner (System.in);
 int a=s.nextInt();
-for(int i=1;i<=a;+++)
+for(int i=1;i<=a;i++)
 {
 System.out.println("Hello");}
 }}
