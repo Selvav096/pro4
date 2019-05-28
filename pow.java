1357dd28ea3db020a02e323d2f38ca215e7dc91b
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 11:39:46 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+import java.util.*;
+public class Main
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt(),b=s.nextInt();double c=Math.pow(a,b);
+System.out.print(c);
+}
+}
+
