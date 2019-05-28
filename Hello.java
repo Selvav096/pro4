3097e21cae1fb48fad69300239f04806c168aca2
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 07:37:23 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+public static void main(String args[]){
+Scanner s=new Scanner (System.in);
+int a=s.nextInt();
+for(int i=1;i<=a;+++)
+{
+System.out.println("Hello");}
+}}
