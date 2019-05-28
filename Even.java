c502c9bfeee6807d9eadc069b49f9d36a8e755b8
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 21:27:06 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main 
+{
+public static void main(String args[])
+{
+int a,b;
+Scanner sc=new Scanner (System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+for(int i=a;i<=b;i++)
+{
+if(i%2==1)
+System.out.print(i+" ");
+}
+}
+}
