b34ae34cf6b943f3b1597aa9f610c7074def309c
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 21:06:16 +0530

    Create Palin.java

---
+++ Palin.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+public staric void main(String args[])
+{
+Scanner s=new Scanner (System.in);
+int a=s.nextInt(),r=0,d,t=a;
+if(a>0)
+{
+while (a>0)
+{
+d=a%10;r=r+d;a=a/10;
+}
+if(t==r)
+System.out.print("yes");
+else 
+System.out.print("no");}
+else{
+System.out.print("Invalid Input");
+}
