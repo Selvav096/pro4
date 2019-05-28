c838cb7f52c6366ba266ac198dd39dd194fa5f65
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 07:11:20 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{ public static void main(String args[])
+{
+Scanner s=new Scanner (System.in);
+int a=s.nextInt(),b=0;
+for(int i=1;i<=a;i++){
+b=b+i;}
+System.out.print(b);
+}}
+
