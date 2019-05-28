938346afa1a22b8ff8eb03638472fc3425585578
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 07:50:49 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+public static void main(String args[])
+{
+String a,b;
+Scanner sc=new Scanner (System.in);
+a=sc.nextLine();
+
+
+System.out.println(String.valueOf(a).length());
+}
+}
