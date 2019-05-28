4b5bc2e151768d152c2a2b6b2e6142063d08ee04
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-28 21:20:53 +0530

    Create Ams.java

---
+++ Ams.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+import java.lang.Math;
+public class Main
+{
+public static void main(String args[])
+{
+int a,b,c,d=0,e=0,f,g;
+Scanner sc=new Scanner (System.in);
+a=sc.nextInt();
+c=a;f=a;
+while(a>0)
+{a=a/10;
+d=d+1;
+
+}
+while(c>0)
+{
+g=c%10;
+e+=Math.pow(g,d);
+c=c/10;
+}
+if(f==e) 
+System.out.println("Yes");
+else
+System.out.println("No");
+}
+}
+}
